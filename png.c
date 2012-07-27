d18812ecfb3b552f95ba156e3ddacf87889f8ac5
Justin <jd116310@ohio.edu>
2012-07-26 22:15:20 -0400

    Chunks are processed with registered functions.

--- png.c
+++ png.c
@@ -14,14 +14,57 @@ typedef struct png_Chunk
 	unsigned int checksum;
 } png_Chunk;
 
+typedef struct png_Type_Callback
+{
+	union
+	{
+		unsigned char type[4];
+		unsigned int type_as_int;
+	};
+	void (*callback)(png_Chunk *chunk);
+} png_Type_Callback;
+
 unsigned int swap32(unsigned int val) 
 {
     return ((val & 0xFF) << 24) | ((val & 0xFF00) << 8) | ((val >> 8) & 0xFF00) | ((val >> 24) & 0xFF);
 }
 
+void process_IHDR(png_Chunk *chunk)
+{
+	printf("Hello from IHDR\n");
+}
+void process_IDAT(png_Chunk *chunk)
+{
+	printf("Hello from IDAT\n");
+}
+void process_IEND(png_Chunk *chunk)
+{
+	printf("Hello from IEND\n");
+}
+
+#define REGISTER_TYPE(x) {#x,*process_##x}
+static png_Type_Callback callbacks[] = 
+{
+	REGISTER_TYPE(IHDR),
+	REGISTER_TYPE(IDAT),
+	REGISTER_TYPE(IEND)
+};
+
 void process_chunk(png_Chunk *chunk)
 {
-	printf("--- %d\n", chunk->type_as_int);
+	int len = sizeof(callbacks) / sizeof(callbacks[0]);
+	int i;
+	for(i = 0; i < len; ++i)
+	{
+		if(callbacks[i].type_as_int == chunk->type_as_int)
+		{
+			(*callbacks[i].callback)(chunk);
+			break;
+		}
+	}
+	
+	if(i == len)
+		printf("Unregistered chunk type %.4s\n", chunk->type);
 }
 
 int main(void)
@@ -57,7 +100,7 @@ int main(void)
 		chunk.checksum = swap32(chunk.checksum);
 		
 		// Print it out
-		printf("%d %.4s\n", chunk.length, chunk.type);
+		//printf("%d %.4s\n", chunk.length, chunk.type);
 		
 		// Process it
 		process_chunk(&chunk);
