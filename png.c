2c2591ccfa2a0580d3abc50e5c8d115a2d9d3441
Justin <jd116310@ohio.edu>
2012-07-28 16:38:15 -0400

    Bare framework to link with zlib.

--- png.c
+++ png.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "zlib.h"
+
 typedef struct png_Chunk
 {
 	unsigned int length;
@@ -68,9 +70,73 @@ void process_IHDR(png_Chunk *chunk)
 	if(header.filter == 0) printf("\t\tFilter: method 0\n");
 	printf("\t\tInterlace: %s\n", header.interlace == 0 ? "none" : "Adam7");
 }
+#define CHUNK 16384*2
+int png_inflate(unsigned char *input, unsigned char *output, int len)
+{
+    int ret;
+    unsigned have;
+    z_stream strm;
+    unsigned char in[CHUNK];
+    unsigned char out[CHUNK];
+
+    /* allocate inflate state */
+    strm.zalloc = Z_NULL;
+    strm.zfree = Z_NULL;
+    strm.opaque = Z_NULL;
+    strm.avail_in = 0;
+    strm.next_in = Z_NULL;
+    ret = inflateInit(&strm);
+    if (ret != Z_OK)
+        return ret;
+/*
+    // /* decompress until deflate stream ends or end of file */
+    // do {
+        // strm.avail_in = fread(in, 1, CHUNK, source);
+        // if (ferror(source)) {
+            // (void)inflateEnd(&strm);
+            // return Z_ERRNO;
+        // }
+        // if (strm.avail_in == 0)
+            // break;
+        // strm.next_in = in;
+
+        // /* run inflate() on input until output buffer not full */
+        // do {
+            // strm.avail_out = CHUNK;
+            // strm.next_out = out;
+            // ret = inflate(&strm, Z_NO_FLUSH);
+            // assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
+            // switch (ret) {
+            // case Z_NEED_DICT:
+                // ret = Z_DATA_ERROR;     /* and fall through */
+            // case Z_DATA_ERROR:
+            // case Z_MEM_ERROR:
+                // (void)inflateEnd(&strm);
+                // return ret;
+            // }
+            // have = CHUNK - strm.avail_out;
+            // if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
+                // (void)inflateEnd(&strm);
+                // return Z_ERRNO;
+            // }
+        // } while (strm.avail_out == 0);
+
+        // /* done when inflate() says it's done */
+    // } while (ret != Z_STREAM_END);
+
+    // /* clean up and return */
+    // (void)inflateEnd(&strm);
+    // return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
+	return 0;
+}
+
 void process_IDAT(png_Chunk *chunk)
 {
 	printf("Hello from IDAT\n");
+	
+	int ret;
+	
+	//ret = png_inflate();
 }
 void process_IEND(png_Chunk *chunk)
 {
