55ac720360dd1bb5eed666720d6b404ccf708745
Justin <jd116310@ohio.edu>
2012-07-28 18:06:58 -0400

    Seems to be inflating data.

--- png.c
+++ png.c
@@ -70,73 +70,67 @@ void process_IHDR(png_Chunk *chunk)
 	if(header.filter == 0) printf("\t\tFilter: method 0\n");
 	printf("\t\tInterlace: %s\n", header.interlace == 0 ? "none" : "Adam7");
 }
-#define CHUNK 16384*2
-int png_inflate(unsigned char *input, unsigned char *output, int len)
-{
-    int ret;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
-
-    /* allocate inflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    strm.avail_in = 0;
-    strm.next_in = Z_NULL;
-    ret = inflateInit(&strm);
-    if (ret != Z_OK)
-        return ret;
-/*
-    // /* decompress until deflate stream ends or end of file */
-    // do {
-        // strm.avail_in = fread(in, 1, CHUNK, source);
-        // if (ferror(source)) {
-            // (void)inflateEnd(&strm);
-            // return Z_ERRNO;
-        // }
-        // if (strm.avail_in == 0)
-            // break;
-        // strm.next_in = in;
-
-        // /* run inflate() on input until output buffer not full */
-        // do {
-            // strm.avail_out = CHUNK;
-            // strm.next_out = out;
-            // ret = inflate(&strm, Z_NO_FLUSH);
-            // assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            // switch (ret) {
-            // case Z_NEED_DICT:
-                // ret = Z_DATA_ERROR;     /* and fall through */
-            // case Z_DATA_ERROR:
-            // case Z_MEM_ERROR:
-                // (void)inflateEnd(&strm);
-                // return ret;
-            // }
-            // have = CHUNK - strm.avail_out;
-            // if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                // (void)inflateEnd(&strm);
-                // return Z_ERRNO;
-            // }
-        // } while (strm.avail_out == 0);
-
-        // /* done when inflate() says it's done */
-    // } while (ret != Z_STREAM_END);
-
-    // /* clean up and return */
-    // (void)inflateEnd(&strm);
-    // return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
-	return 0;
-}
-
+#define ZLIB_CHUNK_SIZE 16384*2
+#define min(a,b) (((a) < (b)) ? (a) : (b))
 void process_IDAT(png_Chunk *chunk)
 {
-	printf("Hello from IDAT\n");
+    static z_stream strm;
+    static unsigned char out[ZLIB_CHUNK_SIZE];
+	
+	static int idat_count = 0;
 	
 	int ret;
+	int bytes_processed = 0;
+	int bytes_output = 0;
+	unsigned char *in;
 	
-	//ret = png_inflate();
+	idat_count++;
+	if(idat_count == 1)
+	{
+		strm.zalloc = Z_NULL;
+		strm.zfree = Z_NULL;
+		strm.opaque = Z_NULL;
+		strm.avail_in = 0;
+		strm.next_in = Z_NULL;
+    
+		ret = inflateInit(&strm);
+		if (ret != Z_OK)
+			printf("inflateInit() failed\n");
+	}
+	if (chunk->length == 0)
+		return;
+	
+	do
+	{
+		strm.avail_in = min(chunk->length, ZLIB_CHUNK_SIZE);
+		strm.next_in = chunk->data + bytes_processed;
+		bytes_processed += strm.avail_in;
+		
+		do
+		{
+			strm.avail_out = ZLIB_CHUNK_SIZE;
+			strm.next_out = out;
+			
+			ret = inflate(&strm, Z_NO_FLUSH);
+			
+			switch (ret)
+			{
+				case Z_NEED_DICT:
+					ret = Z_DATA_ERROR;     /* and fall through */
+				case Z_DATA_ERROR:
+				case Z_MEM_ERROR:
+					printf("inflate() returned %s.\n", strm.msg);
+					(void)inflateEnd(&strm);
+					return;
+			}
+			bytes_output += (ZLIB_CHUNK_SIZE - strm.avail_out);
+		} while (strm.avail_out == 0);
+		
+	} while (ret != Z_STREAM_END);
+	printf("Inflate %d to %d bits\n", chunk->length, bytes_output);
+
+	(void)inflateEnd(&strm);
+	return;
 }
 void process_IEND(png_Chunk *chunk)
 {
