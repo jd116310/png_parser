92d4eb5619aec21d45bc3cc88333a77062c1a112
Justin <jd116310@ohio.edu>
2012-08-12 19:31:52 -0400

    Move code into src directory.

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 set PATH=C:\mingw64\bin;C:\mingw64\lib;%PATH%
 
 del png.exe
-gcc -std=c99 -L. -lzlibwapi debug.c png.c -o png.exe
+gcc -std=c99 -L. -lzlibwapi src/debug.c src/png.c -o png.exe
 
 pause
 
